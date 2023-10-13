d438a8a468ec3b3759416800b4ac82748de61b47
Katsu30 <katsu30@KatsuhironoMacBook-Air.local>
2023-10-13 22:20:38 +0900

    .feat add Header and Footer

---
+++ app/ui/Header/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 border-b z-10 bg-white">
+      <div className="max-w-4xl mx-auto flex justify-between items-center h-12">
+        <Link href="/">LOGO</Link>
+        <div>Link</div>
+      </div>
+    </header>
+  );
+};
